0760d6cc2ff15894292bd12aba98b42c3d552568
Philip Hopkins <phopkins1757@gmail.com>
2025-06-22 20:54:13 -0400

    Deleted _redirects and 200.html and altered vite config to remove copy static files.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,27 +3,11 @@ console.log('✅ vite.config.js is running — Tailwind v4 + static copy active'
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import { resolve } from 'path';
-
-console.log('✅ Output dir:', resolve('dist'));
 
 export default defineConfig({
 	base: '/',
 	plugins: [
 		react(),
 		tailwindcss(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: 'public/_redirects',
-					dest: '.',
-				},
-				{
-					src: 'public/200.html',
-					dest: '.',
-				},
-			],
-		}),
 	],
 });
