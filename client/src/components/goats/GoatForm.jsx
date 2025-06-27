4b244b4c7022249f5390cec3145a1278734659cb
Philip Hopkins <phopkins1757@gmail.com>
2025-06-27 18:08:08 -0400

    Added debugger log to GoatForm.jsx

--- client/src/components/goats/GoatForm.jsx
+++ client/src/components/goats/GoatForm.jsx
@@ -40,6 +40,7 @@ const GoatForm = ({
 	onSubmit,
 	isEdit = false,
 }) => {
+	console.log('🐐 goat.images:', goat?.images);
 	const sensors = useSensors(
 		useSensor(PointerSensor),
 		useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
